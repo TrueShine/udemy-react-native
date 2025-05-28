4dbc80e76ad285812e29eb3dbe429654b6c781ef
Jinuk Ryu <ryu@j1n.uk>
2025-05-28 17:07:10 +0900

    [Section 12] 끝

--- RN_Section12/App.js
+++ RN_Section12/App.js
@@ -10,6 +10,7 @@ import Map from "./screens/Map";
 import { useEffect, useState } from "react";
 import { init } from "./util/database";
 import * as SplashScreen from "expo-splash-screen";
+import PlaceDetails from "./screens/PlaceDetails";
 
 const Stack = createNativeStackNavigator();
 
@@ -67,6 +68,11 @@ export default function App() {
             }}
           />
           <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen
+            name="PlaceDetails"
+            component={PlaceDetails}
+            options={{ title: "Loading place..." }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- RN_Section12/screens/PlaceDetails.js
+++ RN_Section12/screens/PlaceDetails.js
@@ -1,16 +1,48 @@
-import { Image, StyleSheet, View } from "react-native";
-import { ScrollView } from "react-native-web";
+import { useEffect, useState } from "react";
+import { ScrollView, Image, StyleSheet, View, Text } from "react-native";
 import OutlinedButton from "../components/UI/OutlinedButton";
 import { Colors } from "../constants/colors";
 
-function PlaceDetails() {
-  function showOnMapHandler() {}
+import { fetchPlaceDetails } from "../util/database";
+
+function PlaceDetails({ route, navigation }) {
+  const [fetchedPlace, setFetchedPlace] = useState();
+
+  function showOnMapHandler() {
+    navigation.navigate("Map", {
+      initialLat: fetchedPlace.location.lat,
+      initialLng: fetchedPlace.location.lng,
+    });
+  }
+
+  const selectedPlaceId = route.params.placeId;
+
+  useEffect(() => {
+    async function loadPlaceData() {
+      const place = await fetchPlaceDetails(selectedPlaceId);
+      setFetchedPlace(place);
+      navigation.setOptions({
+        title: place.title,
+      });
+    }
+
+    loadPlaceData();
+  }, [selectedPlaceId]);
+
+  if (!fetchedPlace) {
+    return (
+      <View style={styles.fallback}>
+        <Text>Loading place data...</Text>
+      </View>
+    );
+  }
+
   return (
     <ScrollView>
-      <Image />
-      <View>
-        <View>
-          <Text>ADDRESS</Text>
+      <Image style={styles.image} source={{ uri: fetchedPlace.imageUri }} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>{fetchedPlace.address}</Text>
         </View>
         <OutlinedButton icon="map" onPress={showOnMapHandler}>
           View on Map
@@ -23,6 +55,11 @@ function PlaceDetails() {
 export default PlaceDetails;
 
 const styles = StyleSheet.create({
+  fallback: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   screen: {
     alignItems: "center",
   },
